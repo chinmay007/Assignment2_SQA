6b19bbab1594ebb7ede6015f8d811c4fed93ac3e
Chinmay Shah <Cshah5582@conestogac.on.ca>
2021-02-22 01:57:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Assignment2_SQA
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            int ch;
+            do
+            {
+                Console.WriteLine("Enter Your Choice\n 1.Enter Triagnle Dimensions\n 2.Exit");
+                ch = Int32.Parse(Console.ReadLine());
+                switch(ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter The Dimension of the First Side");
+                        a = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter The Dimension of the Second Side");
+                        b = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter The Dimension of the Third Side");
+                        c = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine(TriangleSolver.Analyze(a, b, c));
+                        break;
+                    case 2:
+                        Console.WriteLine("Thank you for using the application");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice Enter Again");
+                        break;
+                }
+            } while (ch != 2);
+        }
+    }
+}
---
+++ TriangleSolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2_SQA
+{
+    public class TriangleSolver
+    {
+        public static string Analyze(int a,int b,int c)
+        {
+            if(a+b<=c || a+c<=b || b+c<=a)
+            {
+                return "Triangle is not Valid";
+            }
+            else
+            {
+                if (a == b && b == c)
+                {
+                    return "This is Equilateral Triangle";
+                }
+                else if (a == b && b == c && a == c)
+                {
+                    return "This is Isosceles Triangle";
+                }
+                else
+                {
+                    return "This is Scalene Triangle";
+                }
+            }
+        }
+    }
+}
